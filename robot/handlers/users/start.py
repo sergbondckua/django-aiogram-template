f4bb69dff8c208fd43ae4c7059cb80520bafceca
sergbondckua <sergninja@gmail.com>
2023-06-14 17:28:59 +0300

    reworked /start

--- robot/handlers/users/start.py
+++ robot/handlers/users/start.py
@@ -2,30 +2,36 @@ import logging
 from datetime import datetime
 
 from aiogram.dispatcher import FSMContext
-from asgiref.sync import sync_to_async
-from django.db import IntegrityError
-
-from robot.models import TelegramUser
-
 from aiogram.types import Message
 from aiogram.dispatcher.filters.builtin import CommandStart
+from asgiref.sync import sync_to_async
+
+from django.db import IntegrityError
 
 from loader import dp
 import const_texts as ct
 from robot.keyboards.default import make_buttons, contact_request_button
 from robot.states.user_register import UserChatRegister
+from robot.models import TelegramUser
+
+
+@dp.message_handler(text_contains="cancel", state=UserChatRegister)
+async def approval(message: Message, state: FSMContext):
+    await message.edit_reply_markup()  # Remove buttons
+    await message.answer("Ви скасували реєстрацію")
+    await state.reset_state()
 
 
 @dp.message_handler(CommandStart())
 async def cmd_start(message: Message):
     telegram_user, _ = await TelegramUser.objects.aget_or_create(
         userid=message.from_user.id,
-        defaults=dict(
-            username=message.from_user.username,
-            first_name=message.from_user.first_name,
-            last_name=message.from_user.last_name,
-            language_code=message.from_user.locale.language,
-        )
+        defaults={
+            "username": message.from_user.username,
+            "first_name": message.from_user.first_name,
+            "last_name": message.from_user.last_name,
+            "language_code": message.from_user.locale.language,
+        }
 
     )
 
@@ -54,7 +60,7 @@ async def cmd_start(message: Message):
 
 
 @dp.message_handler(state=UserChatRegister.phone, content_types="contact")
-async def register_for_telegram(message: Message, state: FSMContext):
+async def register_phone(message: Message, state: FSMContext):
     #  Verification phone number belongs to this account
     if message.contact.user_id != message.from_user.id:
         await message.answer(text=ct.c_share_phone_number_again)
@@ -67,6 +73,8 @@ async def register_for_telegram(message: Message, state: FSMContext):
     await state.update_data(phone=phone_number)
 
     await UserChatRegister.next()
+    await message.reply(
+        text=ct.c_share_phone_number_thx(message.from_user.first_name))
     await message.answer(
         text=ct.c_input_first_name,
         reply_markup=make_buttons(
@@ -75,7 +83,7 @@ async def register_for_telegram(message: Message, state: FSMContext):
 
 
 @dp.message_handler(state=UserChatRegister.first_name)
-async def register_for_telegram(message: Message, state: FSMContext):
+async def register_first_name(message: Message, state: FSMContext):
     await state.update_data(first_name=message.text)
     await UserChatRegister.next()
     await message.answer(
@@ -86,7 +94,7 @@ async def register_for_telegram(message: Message, state: FSMContext):
 
 
 @dp.message_handler(state=UserChatRegister.last_name)
-async def register_for_telegram(message: Message, state: FSMContext):
+async def register_last_name(message: Message, state: FSMContext):
     await state.update_data(last_name=message.text)
     await UserChatRegister.next()
     await message.answer(
@@ -97,7 +105,7 @@ async def register_for_telegram(message: Message, state: FSMContext):
 
 @dp.message_handler(
     state=UserChatRegister.birthday, regexp=r"^\d{2}\.\d{2}\.\d{4}$")
-async def register_for_telegram(message: Message, state: FSMContext):
+async def register_birthday(message: Message, state: FSMContext):
     try:
         birth_date = datetime.strptime(message.text, "%d.%m.%Y")
     except ValueError:
--- robot/keyboards/default/user_register.py
+++ robot/keyboards/default/user_register.py
@@ -3,7 +3,10 @@ from const_texts import c_share_phone_number, c_cancel
 
 
 def make_buttons(words: list, row_width: int = 1) -> ReplyKeyboardMarkup:
-    buttons_group = ReplyKeyboardMarkup(row_width=row_width, resize_keyboard=True)
+    buttons_group = ReplyKeyboardMarkup(
+        row_width=row_width,
+        resize_keyboard=True,
+    )
     for word in words:
         if word is not None:
             buttons_group.insert(KeyboardButton(text=word))
